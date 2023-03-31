7c845d949e417a5190b30568b1235d74eb768085
Kevin Yang <kevin7616@gmail.com>
2023-03-31 15:56:07 +1300

    Add files via upload
    Initial commit

---
+++ src/pages/listOfItems/ListOfItems.jsx
@@ -0,0 +1,18 @@
+import Sidebar from "../../components/sidebar/Sidebar"
+import Navbar from "../../components/navbar/Navbar"
+import Datatable from "../../components/datatable/Datatable"
+import "./listOfItems.scss";
+
+const ListOfItems = () => {
+  return (
+    <div className="list">
+      <Sidebar/>
+      <div className="listContainer">
+        <Navbar/>
+        <Datatable/>
+      </div>
+    </div>
+  )
+}
+
+export default ListOfItems
